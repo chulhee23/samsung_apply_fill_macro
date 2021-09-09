06d13b1a47f223b320f253be664ea2c7b3d39b1a
chulhee23 <chulhee2351@gmail.com>
2021-09-09 15:03:27 +0900

    2021 ver

---
+++ README.md
@@ -0,0 +1,38 @@
+# samsung_apply_fill_macro
+> 2021 하반기 채용
+사용 시, 
+```python run.py data.xlsx```
+입력 후 바로 크롬에서 이수교과목 탭 - 과정 클릭해서 focus 준 상태로 기다리면
+알아서 엑셀에 들어간다.
+해당 위치에서 tab으로 이동하기 때문에 크롬창에서 클릭한다던지 이동작업 없어야함!
+
+삼성 채용 성적 입력 매크로  
+테스트 환경: `Windows10 Chrome`  
+
+정수 단위 학점 외 입력은 수동으로 하여야 합니다.  
+리눅스에서 작동하지 않습니다.  
+
+본 프로그램으로 발생하는 불이익은 저작권자가 책임지지 않습니다.  
+
+# How to use
+
+1. python3.6 설치. 예시링크(https://winpython.github.io/)
+
+2. pip install -r requirements.txt
+
+3. data.xlsx 형식으로 성적 엑셀 입력  
+   학교란은 편입한 경우 학적에 뜨는 학교 이름 순서대로 1번 2번 ...  
+   해당사항이 없는 경우는 그냥 1로 적으면 된다.  
+
+4. 미리 삼성 채용 홈페이지-채용 지원서 작성-이수교과목 페이지 접속
+
+5. run.py "엑셀 파일" 실행  
+   ex) run.py data.xlsx  
+   ex) run.py seoul.xlsx  
+
+6. 프로그램 실행 후, config.yaml 파일의 setup_delay_time 안에 과목 입력란 중 과정 누르고 엔터  
+   매크로 실행 중 중간에 중단되어 과목 수 카운트 수가 달라진다면 config.yaml 파일의 start_num 수정
+![](docs/1.PNG)
+![](docs/2.PNG)
+7. 네트워크 지연속도나 안정성을 고려한다면 config.yaml 파일의 delay_time 조절  
+   학교명 서버 데이터를 가져오는 것이 느리므로 안정적으로 실행하려면 run.py 내의 서버 딜레이를 1초로 설정
---
+++ run.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+import time
+import sys
+import pandas as pd
+import pyperclip
+import yaml
+
+import macro
+
+def load_config():
+    with open('config.yaml', encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    return config
+
+arg = sys.argv[1]
+print(arg)
+config = load_config()
+delay_time = config["delay_time"]
+bot = macro.ChatMacro(delay_time)
+data = pd.read_excel(arg, engine='openpyxl')
+start_num = config["start_num"] #첫번째를 0으로 기준, 중간에 끊기면 이 숫자 조절
+setup_delay_time = config["setup_delay_time"]
+class_taken_year = config["class_taken_year"]
+degree_type = config["degree_type"]
+
+def next_form(bot):
+    bot.press("tab")
+    bot.press("tab")
+    bot.press("enter")
+
+time.sleep(setup_delay_time) #3초내로 과정 탭 누르고 엔터클릭
+for index, row in data[start_num:].iterrows():
+    bot.press("enter") #과정
+    for i in range(degree_type-1):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot)
+    
+    time.sleep(1) #전공명 서버 딜레이
+    univ_type = row["학교"]
+    for _ in range(1, univ_type):
+        bot.press('tab')
+    bot.press("enter") #전공명
+    next_form(bot)
+
+    year = row["수강년도"]  # 수강년도
+    count = class_taken_year - year
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot)
+
+    semester = row["학기"]  # 학기
+    if isinstance(semester, int):
+
+        count = semester - 1
+    elif semester.startswith("여름"):
+        count = 3
+    elif semester.startswith("겨울"):
+        count = 4
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot)
+
+    
+    class_type = row["과목유형"] #과목유형
+    if class_type.startswith("교양"):
+        count = 1
+    elif class_type.startswith("전공"):
+        count = 0
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot)
+
+    is_retaken = row["재수강"]  # 재수강여부
+    if not is_retaken:
+        count = 1
+    else:
+        count = 0
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    bot.press("tab")
+    bot.press("tab")
+    bot.press("enter")
+
+
+    
+    class_name = row["과목명"] #과목명
+    pyperclip.copy(class_name)
+    bot.pressHoldRelease("ctrl", "v")
+    bot.press("tab")
+    bot.press("enter")
+    
+    class_point = row["취득학점"] #취득학점
+    count = class_point-1
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot)
+
+
+    class_grade = row["성적"] #성적
+    grade_dict = {"A+": 0,
+                  "A": 1,
+                  "A-": 2,
+                  "B+": 3,
+                  "B": 4,
+                  "B-": 5,
+                  "C+": 6,
+                  "C": 7,
+                  "C-": 8,
+                  "D+": 9,
+                  "D": 10,
+                  "D-": 11,
+                  "F": 12,
+                  "PASS": 13,
+                  "P": 13,
+                  "FAIL": 14,
+                  "기타": 15}
+    count = grade_dict[class_grade]
+    for i in range(count):
+        bot.press("down_arrow")
+    bot.press("enter")
+    next_form(bot) #추가버튼
+    
+    
+    if index%10 == 9: #10단위 중간 저장
+        time.sleep(1) #딜레이
+        bot.press("enter") #중간 저장 팝업
+        time.sleep(1)
+    time.sleep(0.2) #추가버튼 지연
+    bot.press("tab")
+    bot.press("enter") #내용삭제
+    for i in range(17): #원 상태로 복귀
+        bot.pressHoldRelease("shift", "tab")
+        
+        
